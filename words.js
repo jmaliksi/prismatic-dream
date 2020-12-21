992583ab7cc76318a865351c2c5d6616ce012393
joe <joe.maliksi@gmail.com>
2020-12-20 19:31:37 -0800

    refactor main.js

--- words.js
+++ words.js
@@ -1,4 +1,5 @@
 const data = require('./words.json');
+const tracery = require('tracery-grammar');
 
 function hasTag(word, tag) {
   if (!data[word]) {
@@ -46,10 +47,163 @@ function taggedBy(...tags) {
 
 stats(tagTree);
 //console.log(taggedBy('noun', 'uncountable'));
+var dreamSequences = [
+  '#dream# #dream#',
+  '#dream# #dream# #dream#',
+  '#dream# #dream# #dream# #dream#',
+  //'#dream#\n #dream#\n #dream#\n #dream#\n #dream#',
+];
+
+var dreams = [
+  '#subjectClause.capitalize# #preposition# #nounPhrase#.',
+  '#subjectClause.capitalize# #preposition# #nounPhrase#, #noun.s# #verb.ing# #preposition# #nounPhrase#.',
+  '#subjectClause.capitalize# #preposition# #nounPhrase#, #noun.s# #verb.ing# #preposition# #nounPhrase#.',
+  'There is #singularNounPhrase#.',
+  'There are #pluralNounPhrase#.',
+  'The #nounness# of #nounPhrase#.',
+  '#subjectClause.capitalize#.',
+  '#countableNoun.s.capitalize# #verb#, #countableNoun.s# #verb#, #countableNoun.s# #verb#.',
+  //'The #noun# #verb.s#, the #noun# #verb.s#, the #noun# #verb.s#.',
+  'Memories of #nounPhrase#.',
+  'Memories of #nounPhrase#, #verb.ing# #preposition# #nounPhrase#.',
+  '#verb.ing.a.capitalize# #adjective# #countableNoun#.',
+  'What #verb.s# #preposition# #nounPhrase#?'
+];
+
+var dreamSubject = ['#prevNoun.capitalize# #action#'];
+
+var dreamAction = [
+  '#dreamSubject.sif#.',
+  '#dreamSubject.sif#.',
+  '#dreamSubject.sif# #preposition# #nounPhrase#.',
+  '#dreamSubject.sif# #preposition# #nounPhrase#.',
+  //'#dreamSubject.sif#, #singularNounPhrase# #verb.ing# #preposition# #nounPhrase#.',
+  //'#dreamSubject.sif#, #pluralNounPhrase# #verb.ing# #preposition# #nounPhrase#.'
+]
+
+var relationship = [
+  '#relationAdjective# #relation#',
+]
+
+var subjectClauses = [
+  '#singularNounPhrase# #verb.s#',
+  '#pluralNounPhrase# #verb#'
+];
+
+var singularNounPhrases = [
+  '#uncountableNoun#',
+  '#uncountableNoun#',
+  //'#adjective# #uncountableNoun#',
+  'the #noun#',
+  'the #noun#',
+  'the #adjective# #noun#',
+  'the #adjective# #noun#',
+  '#countableNoun.a#',
+  '#countableNoun.a#',
+  '#adjective.a# #noun#',
+  '#adjective.a# #noun#',
+  'some #uncountableNoun#',
+  'some #adjective# #uncountableNoun#'
+];
+
+var pluralNounPhrases = [
+  '#countableNoun.s#',
+  '#adjective# #countableNoun.s#',
+  '#countableNoun.s#',
+  '#adjective# #countableNoun.s#',
+  '#countableNoun.s#',
+  '#adjective# #countableNoun.s#',
+  'some #countableNoun.s#',
+  'a few #countableNoun.s#',
+  'many #countableNoun.s#',
+  'several #countableNoun.s#',
+  'inummerable #countableNoun.s#',
+  'countless #countableNoun.s#',
+];
+
+var baseGrammar = {
+  'dreamSequence': dreamSequences,
+  'dream': dreams,
+  'noun': tagTree.noun.concat(relationship),
+  'nounness': tagTree.nounness,
+  'verb': tagTree.verb,
+  'preposition': tagTree.preposition,
+  'subjectClause': subjectClauses,
+  'adjective': tagTree.adjective,
+  'countableNoun': taggedBy('noun', 'countable').concat(relationship),
+  'uncountableNoun': taggedBy('noun', 'uncountable'),
+  'singularNounPhrase': singularNounPhrases,
+  'pluralNounPhrase': pluralNounPhrases,
+  'nounPhrase': ['#singularNounPhrase#', '#pluralNounPhrase#'],
+  'dreamAction': dreamAction,
+  'dreamSubject': dreamSubject,
+  'upVerb': taggedBy('verb', 'up'),
+  'downVerb': taggedBy('verb', 'down'),
+  'relationAdjective': taggedBy('adjective', 'relational'),
+  'relation': tagTree.relation
+};
+
+var mods = {...tracery.baseEngModifiers};
+var originalSMod = mods.s;
+mods.s = function(s) {
+  if (s.slice(-5) === 'child') {
+    return s.slice(0, -5) + 'children';
+  }
+  if (s.slice(-5) === 'mouse') {
+    return s.slice(0, -5) + 'mice';
+  }
+  if (s.slice(-5) === 'laugh') {
+    return s.slice(0, -5) + 'laughs';
+  }
+  if (s.length >= 3 && s.slice(-3) === 'man') {
+    return s.slice(0, -3) + 'men';
+  }
+  if (s.length >= 6 && s.slice(-6) === 'tomato') {
+    return s.slice(0, -6) + 'tomatoes';
+  }
+  if (s.length >= 5 && s.slice(-5) === 'knife') {
+    return s.slice(0, -5) + 'knives';
+  }
+  if (s.length >= 5 && s.slice(-5) === 'person') {
+    return s.slice(0, -5) + 'knives';
+  }
+  return originalSMod(s);
+};
+mods.ing = function(s) {
+  if (s.charAt(s.length - 1) === 'e') {
+    return s.slice(0, -1) + 'ing';
+  }
+  else if (s.slice(-3) === 'set') {
+    return s.slice(0, -3) + 'setting';
+  }
+  else if (s.slice(-3) === 'run') {
+    return s.slice(0, -3) + 'running';
+  }
+  else if (s.slice(-4) === 'drop') {
+    return s.slice(0, -4) + 'dropping';
+  }
+  else if (s.slice(-4) === 'drop') {
+    return s.slice(0, -4) + 'dropping';
+  }
+  else if (s.slice(-3) === 'cut') {
+    return s.slice(0, -3) + 'cutting';
+  }
+  return s + 'ing';
+}
+mods.sif = function(s) {
+  s2 = s.split(" ");
+  var noun = s2[s2.length - 2];
+  if (noun[noun.length - 1] !== 's') {
+    return mods.s(s);
+  }
+  return s;
+}
 
 module.exports = {
   'tagTree': tagTree,
   'data': data,
   'hasTag': hasTag,
-  'taggedBy': taggedBy
+  'taggedBy': taggedBy,
+  'baseGrammar': baseGrammar,
+  'mods': mods
 };
